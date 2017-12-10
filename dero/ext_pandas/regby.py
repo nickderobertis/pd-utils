fd367f2ae776af010ed094484e461b4ba96824f3
Nick DeRobertis <whoopnip@gmail.com>
2017-12-10 18:50:35 -0500

    Final restructuring of reg_by before going to multiprocessing

--- dero/ext_pandas/regby.py
+++ dero/ext_pandas/regby.py
@@ -32,38 +32,16 @@ def reg_by(df, yvar, xvars, groupvar, merge=False, cons=True):
     # Split DataFrame into a list of arrays with each bygroup being one array. Provide an accompanying list of bygroups
     arrs, groups = _get_lists_of_arrays_and_groups(yx_df, yvar, xvars, groupvar)
 
-    results = []
-    for i, arr in enumerate(arrs):
-        X = arr[:, 1:].astype(float)
+    # Run regressions by groups, storing results as a list of numpy arrays
+    results = _reg_by(arrs, groups, xvars, rhs, cons)
 
-        if cons:
-            X = sm.add_constant(X)
-
-        y = arr[:, 0].astype(float)
-
-        if arr.shape[0] > len(xvars) + 1:  # if enough observations, run regression
-            model = sm.OLS(y, X)
-            result = model.fit()
-            this_result = np.append(result.params, groups[i]) # add groupvar
-            this_result = this_result[None, :] # cast 1d array into 2d array
-        else:  # not enough obs, return nans
-            this_result = np.empty((1, len(rhs) + 1), dtype='O')
-            this_result[:] = nan
-            this_result[0, len(rhs)] = groups[i]
-
-        results.append(this_result)
-
-    result_df = pd.DataFrame(np.concatenate(results, axis=0))
-    result_df = result_df.apply(pd.to_numeric, errors='ignore')
-    cols = rhs + [groupvar]
-    result_df.columns = ['coef_' + col if col not in (groupvar, 'const') else col for col in cols]
+    # Combine list of arrays into df, and apply column labels
+    result_df = _result_list_of_arrays_to_df(results, rhs, groupvar)
 
     if merge:
         result_df = df.merge(result_df, how='left', on=groupvar)
     if drop_group:
         result_df.drop(groupvar, axis=1, inplace=True)
-
-
 
     return result_df.reset_index(drop=True)
 
@@ -74,24 +52,32 @@ def df_for_reg(df, yvar, xvars, groupvar):
     yx_df = pd.concat([yx_df, df[groupvar]], axis=1).dropna()
 
     return yx_df
-#
-# def _reg(arr, result_df, xvars, cons, groupvar, group):
-#     X = arr[:, 1:].astype(float)
-#
-#     if cons:
-#         X = sm.add_constant(X)
-#
-#     y = arr[:, 0].astype(float)
-#
-#     if arr.shape[0] > len(xvars) + 1:  # if enough observations, run regression
-#         model = sm.OLS(y, X)
-#         result = model.fit()
-#         this_result = pd.DataFrame(result.params).T
-#     else:  # not enough obs, return nans
-#         this_result = pd.DataFrame(data=[nan for i in range(len(rhs))]).T
-#
-#     this_result[groupvar] = groups[i]
-#     result_df = result_df.append(this_result)  # Or whatever summary info you want
+
+def _reg_by(arrs, groups, xvars, rhs, cons):
+    results = []
+    for i, arr in enumerate(arrs):
+        results.append(_reg(arr, xvars, rhs, cons, groups[i]))
+    return results
+
+def _reg(arr, xvars, rhs, cons, group):
+    X = arr[:, 1:].astype(float)
+
+    if cons:
+        X = sm.add_constant(X)
+
+    y = arr[:, 0].astype(float)
+
+    if arr.shape[0] > len(xvars) + 1:  # if enough observations, run regression
+        model = sm.OLS(y, X)
+        result = model.fit()
+        this_result = np.append(result.params, group)  # add groupvar
+        this_result = this_result[None, :]  # cast 1d array into 2d array
+    else:  # not enough obs, return nans
+        this_result = np.empty((1, len(rhs) + 1), dtype='O')
+        this_result[:] = nan
+        this_result[0, len(rhs)] = group
+
+    return this_result
 
 
 def _check_inputs_regby(xvars):
@@ -119,6 +105,14 @@ def _set_rhs(xvars, cons):
         rhs = xvars
 
     return rhs
+
+def _result_list_of_arrays_to_df(results, rhs, groupvar):
+    result_df = pd.DataFrame(np.concatenate(results, axis=0))
+    result_df = result_df.apply(pd.to_numeric, errors='ignore')
+    cols = rhs + [groupvar]
+    result_df.columns = ['coef_' + col if col not in (groupvar, 'const') else col for col in cols]
+
+    return result_df
 
 def _get_lists_of_arrays_and_groups(df, yvar, xvars, groupvar):
     arrs = split(df, [yvar] + xvars, keyvar=groupvar)
