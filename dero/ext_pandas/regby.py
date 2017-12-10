f0d26401177025534f54c4cc409cf6f4ba46bf02
Nick DeRobertis <whoopnip@gmail.com>
2017-12-10 17:48:52 -0500

    More restructuring of reg_by, for readability and separation of logic

--- dero/ext_pandas/regby.py
+++ dero/ext_pandas/regby.py
@@ -16,33 +16,22 @@ def reg_by(df, yvar, xvars, groupvar, merge=False, cons=True):
     Optional Options:
     cons: True to include a constant, False to not
     """
+    # Convert xvars to list if str is passed
+    xvars = _check_inputs_regby(xvars)
+
+    # If there are multiple groupvars, create a key variable containing all groupvars (modifies df inplace)
+    groupvar, drop_group = _set_groupvar_and_drop_group(df, groupvar)
+
+    # Select dataframe of only y, x and group vars and drop any missings
+    yx_df = df_for_reg(df, yvar, xvars, groupvar)
+
+    # Create a list of right hand side variables. Includes 'const' if cons is True
+    rhs = _set_rhs(xvars, cons)
+
+    # Split DataFrame into a list of arrays with each bygroup being one array. Provide an accompanying list of bygroups
+    arrs, groups = _get_lists_of_arrays_and_groups(yx_df, yvar, xvars, groupvar)
+
     result_df = pd.DataFrame()
-
-    if isinstance(xvars, str):
-        xvars = [xvars]
-    assert isinstance(xvars, list)
-
-    drop_group = False
-    if isinstance(groupvar, list):
-        df['__key_regby__'] = ''
-        for var in groupvar:
-            df['__key_regby__'] = df['__key_regby__'] + df[var].astype(str)
-        groupvar = '__key_regby__'
-        drop_group = True
-
-    # Select dataframe of only y and x vars
-    yx_df = df.loc[:, xvars + [yvar]]
-    # Recombine groupvar and drop missing
-    yx_df = pd.concat([yx_df, df[groupvar]], axis=1).dropna()
-
-    if cons:
-        rhs = ['const'] + xvars
-    else:
-        rhs = xvars
-
-    arrs = split(yx_df, [yvar] + xvars, keyvar=groupvar)
-    groups = df[groupvar].unique().tolist()
-    assert len(arrs) == len(groups)
 
     for i, arr in enumerate(arrs):
         X = arr[:, 1:].astype(float)
@@ -71,3 +60,63 @@ def reg_by(df, yvar, xvars, groupvar, merge=False, cons=True):
         result_df.drop(groupvar, axis=1, inplace=True)
 
     return result_df.reset_index(drop=True)
+
+def df_for_reg(df, yvar, xvars, groupvar):
+    # Select dataframe of only y and x vars
+    yx_df = df.loc[:, xvars + [yvar]]
+    # Recombine groupvar and drop missing
+    yx_df = pd.concat([yx_df, df[groupvar]], axis=1).dropna()
+
+    return yx_df
+#
+# def _reg(arr, result_df, xvars, cons, groupvar, group):
+#     X = arr[:, 1:].astype(float)
+#
+#     if cons:
+#         X = sm.add_constant(X)
+#
+#     y = arr[:, 0].astype(float)
+#
+#     if arr.shape[0] > len(xvars) + 1:  # if enough observations, run regression
+#         model = sm.OLS(y, X)
+#         result = model.fit()
+#         this_result = pd.DataFrame(result.params).T
+#     else:  # not enough obs, return nans
+#         this_result = pd.DataFrame(data=[nan for i in range(len(rhs))]).T
+#
+#     this_result[groupvar] = groups[i]
+#     result_df = result_df.append(this_result)  # Or whatever summary info you want
+
+
+def _check_inputs_regby(xvars):
+    if isinstance(xvars, str):
+        xvars = [xvars]
+    assert isinstance(xvars, list)
+
+    return xvars
+
+def _set_groupvar_and_drop_group(df, groupvar):
+    drop_group = False
+    if isinstance(groupvar, list):
+        df['__key_regby__'] = ''
+        for var in groupvar:
+            df['__key_regby__'] = df['__key_regby__'] + df[var].astype(str)
+        groupvar = '__key_regby__'
+        drop_group = True
+
+    return groupvar, drop_group
+
+def _set_rhs(xvars, cons):
+    if cons:
+        rhs = ['const'] + xvars
+    else:
+        rhs = xvars
+
+    return rhs
+
+def _get_lists_of_arrays_and_groups(df, yvar, xvars, groupvar):
+    arrs = split(df, [yvar] + xvars, keyvar=groupvar)
+    groups = df[groupvar].unique().tolist()
+    assert len(arrs) == len(groups)
+
+    return arrs, groups
