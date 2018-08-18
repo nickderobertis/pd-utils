653c482c17a93e16f5ebaf9a0f0805b68a67a6ab
Nick DeRobertis <whoopnip@gmail.com>
2018-08-18 11:34:04 -0400

    Fix where optimize dtypes would fail if a dtype was not included in the df

--- dero/ext_pandas/optimize/dftypes.py
+++ dero/ext_pandas/optimize/dftypes.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from dero.ext_pandas.optimize import PdDTypeQuadTuple
+from dero.ext_pandas.optimize.typing import PdDTypeQuadTuple, StrDict
 
 
 def optimized_df(df: pd.DataFrame) -> pd.DataFrame:
@@ -12,13 +12,16 @@ def optimized_df(df: pd.DataFrame) -> pd.DataFrame:
 
 def _optimized_dtype_dfs(df: pd.DataFrame) -> PdDTypeQuadTuple:
     obj_df = df.select_dtypes(include=['object'])
-    obj_df = obj_df.astype('category')
+    if not obj_df.empty:
+        obj_df = obj_df.astype('category')
 
     int_df = df.select_dtypes(include=['int'])
-    int_df = int_df.apply(pd.to_numeric, downcast='unsigned')
+    if not int_df.empty:
+        int_df = int_df.apply(pd.to_numeric, downcast='unsigned')
 
     float_df = df.select_dtypes(include=['float'])
-    float_df = float_df.apply(pd.to_numeric, downcast='float')
+    if not float_df.empty:
+        float_df = float_df.apply(pd.to_numeric, downcast='float')
 
     type_dfs = (obj_df, int_df, float_df)
 
@@ -30,3 +33,13 @@ def _optimized_dtype_dfs(df: pd.DataFrame) -> PdDTypeQuadTuple:
     excluded_columns = [col for col in df.columns if col not in optimized_columns]
 
     return type_dfs + (df[excluded_columns],)
+
+def df_types_dict(df: pd.DataFrame, remove_dates=True) -> StrDict:
+    df_types_dict = _df_types_dict(df)
+    if remove_dates:
+        return {col_name: dtype for col_name, dtype in df_types_dict.items() if 'date' not in dtype}
+    else:
+        return df_types_dict
+
+def _df_types_dict(df: pd.DataFrame) -> StrDict:
+    return df.dtypes.apply(lambda x: str(x)).to_dict()
--- dero/ext_pandas/optimize/typing.py
+++ dero/ext_pandas/optimize/typing.py
@@ -1,6 +1,7 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, Dict
 
 import pandas as pd
 
 DfOrSeries = Union[pd.DataFrame, pd.Series]
 PdDTypeQuadTuple = Tuple[DfOrSeries, DfOrSeries, DfOrSeries, DfOrSeries]
+StrDict = Dict[str, str]
